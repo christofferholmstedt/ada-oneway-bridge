b794bb5d6e32f152fe485a3bf1753425d64d1941
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-10-09 07:38:48 +0200

    Fixed all warnings in Wikibooks Ada tutorial example hello_world_2

--- ada_tutorial/hello_world_2.adb
+++ ada_tutorial/hello_world_2.adb
@@ -1,9 +1,9 @@
-with Ada.Text.IO
+with Ada.Text_IO;
 
-procedure Hello is
+procedure Hello_World_2 is
     package IO renames Ada.Text_IO;
 begin
     IO.Put_Line("Hello, world!");
     IO.New_Line;
     IO.Put_Line("I am an Ada program with package rename.");
-end Hello;
+end Hello_World_2;
