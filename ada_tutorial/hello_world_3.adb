ecd5f5b6fc3a110d94d2ba50477e138f85b34ce3
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-10-09 07:49:41 +0200

    Added third example from Wikibooks Ada tutorial

---
+++ ada_tutorial/hello_world_3.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Hello_World_3 is
+
+begin
+    Put_Line("Hello, world!");
+    New_Line;
+    Put_Line("I am an Ada program with package use.");
+end Hello_World_3;
